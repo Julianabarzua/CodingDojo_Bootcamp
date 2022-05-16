d2e7ba87df3f432009b14cdf846eed120ae866fd
julianabarzua <julianabarzua@gmail.com>
2022-05-16 18:18:56 -0400

    Uploading cat vs dog excersise

---
+++ 01_Web_Fundamentals/03_JS/04_Ex_Cats_vs_Dog/script.js
@@ -0,0 +1,12 @@
+var animalImg = document.querySelector("#fav-animal")
+console.log(animalImg)
+
+
+
+function pickCat() {
+    animalImg.src = "Cat.png"
+}
+
+function pickDog() {
+    animalImg.src = "Dog.png"
+}
