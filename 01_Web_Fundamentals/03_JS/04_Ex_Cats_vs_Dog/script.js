c9512933fcdf734d31c0375cd2e7e338787039ee
julianabarzua <julianabarzua@gmail.com>
2022-05-16 18:58:41 -0400

    Uploading changes to dogvscat

--- 01_Web_Fundamentals/03_JS/04_Ex_Cats_vs_Dog/script.js
+++ 01_Web_Fundamentals/03_JS/04_Ex_Cats_vs_Dog/script.js
@@ -1,12 +1,27 @@
 var animalImg = document.querySelector("#fav-animal")
-console.log(animalImg)
+// console.log(animalImg)
 
 
 
-function pickCat() {
+function pickCat(element) {
+    // console.log(element.style)
+    element.style.backgroundColor = "goldenrod";
     animalImg.src = "Cat.png"
 }
 
-function pickDog() {
+function pickDog(element) {
+    element.classList.add("btn");
     animalImg.src = "Dog.png"
+}
+
+function setActive(element) {
+    var background = document.querySelector("body")
+    if(background.classList.contains("dark-mode")) {
+        element.innerText = "Cambiar al modo oscuro";
+        background.classList.remove("dark-mode");
+    } else {
+        element.innerText = "Cambiar al modo claro";        
+        background.classList.add("dark-mode");
+    }
+    console.log(element.classList)
 }
