9f0af3a79b5ee0c7402367fe590aaa454959fcc2
julianabarzua <julianabarzua@gmail.com>
2022-05-19 21:31:37 -0400

    uploading loops challenge

---
+++ 01_Web_Fundamentals/03_JS/09_Prac_Loops_chanllenge/script.js
@@ -0,0 +1,26 @@
+
+for (let i=1; i<=20; i+=2 ){
+    console.log(i);
+}
+
+for (let i=99; i>=0; i-=3 ){
+    console.log(i);
+}
+
+let list = [4, 2.5, 1, -0.5, -2, -3.5];
+
+for (let i=0; i<list.length; i++ ){
+    console.log(list[i]);
+}
+
+let sum=0;
+for (let i=1; i<=100; i++){
+    sum +=i;
+}
+console.log(sum)
+
+let product=1;
+for (let i=1; i<=12; i++){
+    product *=i;
+}
+console.log(product)
