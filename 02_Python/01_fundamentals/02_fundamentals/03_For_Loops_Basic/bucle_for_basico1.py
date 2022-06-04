d89fbf9a349b9f315a652c217a7b7031ad0ecdbb
julianabarzua <julianabarzua@gmail.com>
2022-06-04 09:26:14 -0400

    uploading for loops basic

---
+++ 02_Python/01_fundamentals/02_fundamentals/03_For_Loops_Basic/bucle_for_basico1.py
@@ -0,0 +1,47 @@
+
+
+# #imprime todos los numeors del 0 al 150
+# for x in range(151):
+#     print(x)
+
+
+# #impirme los multiplos de 5 entre 5 y 1000
+# for x in range(5,1001,5):
+#     print(x)
+
+
+
+# #imprime enteros del 1 al 100, reemplazando multiplos de 5 por "coding" y de 10 por "coding dojo"
+# for x in range(1,101):
+    
+#     if x%10 == 0:
+#         print("Coding Dojo")
+#     elif x%5 == 0:
+#         print("coding")
+#     else:
+#         print(x)
+
+#suma impares desde 1 hasta 500000
+# sum=0
+# for x in range(1,500001,2):
+#     sum +=x
+#     print(x)
+
+# print(sum)
+
+# #imprime números positivos comenzando desde el 2018, en cuenta regresiva de 4 en 4.
+# for x in range(2018,0,-4):
+#     print(x)
+
+#Comenzando en lowNum y pasando por highNum, imprime solo los enteros que sean múltiplos de mult.
+lowNum = int(input('Ingrese lowNum: '))
+highNum = int(input('Ingrese highNum: '))
+mult = int(input('Ingrese multiplicador: '))
+
+for x in range(lowNum,highNum+1):
+    if x%mult == 0:
+        print(x)
+
+
+
+
