a75d7c6fb76e73343b8e3f540cd9a2106c4a5fa2
julianabarzua <julianabarzua@gmail.com>
2022-06-28 14:25:24 -0400

    uploading CRUD users

---
+++ 02_Python/04_flask_mysql/01_db_connection/03_CRUD_Users/server.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect
+# importar la clase de friend.py
+from users import User
+app = Flask(__name__)
+
+@app.route("/")
+def mainroute():
+    return redirect("/users")
+
+@app.route("/users")
+def index():
+    # llamar al método de clase get all para obtener todos los amigos
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", users = users)
+            
+
+@app.route("/users/<id>")
+def show(id):
+    # llamar al método de clase get all para obtener todos los amigos
+    user = User.get_one(id=id)
+    print(user)
+    return render_template("show.html", user = user)
+
+@app.route("/users/<id>/destroy")
+def delete(id):
+    # llamar al método de clase get all para obtener todos los amigos
+    User.delete(id=id)
+    return redirect("/")
+
+@app.route("/users/<id>/edit")
+def edit(id):
+    user = User.get_one(id=id)
+    return render_template("edit.html",user=user)
+
+@app.route("/users/new")
+def new():
+
+    return render_template("new_user.html")
+
+# fragmento de código relevante de server.py
+@app.route('/create_user', methods=["POST"])
+def create_friend():
+    # Primero hacemos un diccionario de datos a partir de nuestro request.form proveniente de nuestra plantilla
+    # Las claves en los datos tienen que alinearse exactamente con las variables en nuestra cadena de consulta
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    # Pasamos el diccionario de datos al método save de la clase Friend
+    User.save(data)
+    # No olvides redirigir después de guardar en la base de datos
+    return redirect('/users')
+
+@app.route('/edit_user', methods=["POST"])
+def edit_fuser():
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    User.update(data)
+    return redirect('/users')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
