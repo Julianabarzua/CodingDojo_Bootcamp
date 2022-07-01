a27a017964ce8ed51414674599025da548a36b8f
julianabarzua <julianabarzua@gmail.com>
2022-07-01 19:46:25 -0400

    uploading dojos and ninjas

---
+++ 02_Python/04_flask_mysql/02_crud/03_Dojos_and_ninjas/flask_app/controllers/ninjas_cont.py
@@ -0,0 +1,57 @@
+from flask import render_template, request, redirect
+# importar la clase de friend.py
+
+from flask_app import app
+# ...server.py
+
+from flask_app.models.ninjas_model import Ninja
+from flask_app.models.dojos_model import Dojo
+
+
+@app.route("/")
+def mainroute():
+    return redirect("/dojos")
+
+@app.route("/dojos")
+def index():
+    # llamar al método de clase get all para obtener todos los amigos
+    dojos = Dojo.get_all()
+    print(dojos)
+    return render_template("index.html", dojos=dojos)
+
+@app.route("/create_dojo",  methods=["POST"])
+def create_dojo():
+    data = {
+        "name": request.form["name"]
+    }
+    Dojo.save(data)
+    return redirect("/dojos")
+
+@app.route("/dojos/<id>")
+def show_dojo(id):
+
+    data = {
+        "id": id
+    }
+    dojo = Dojo.get_dojo_with_ninjas(data)
+
+    return render_template("show.html", dojo=dojo)
+
+@app.route("/ninjas")
+def new_ninja():
+    dojos = Dojo.get_all()
+    print(dojos)
+    return render_template('new_ninja.html', dojos=dojos)
+
+
+@app.route("/create_ninja",  methods=["POST"])
+def create_ninja():
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "age" : request.form["age"],
+        "dojo_id" : request.form["dojo"]
+    }
+
+    Ninja.save(data)
+    return redirect("/")
---
+++ 02_Python/04_flask_mysql/02_crud/03_Dojos_and_ninjas/flask_app/models/ninjas_model.py
@@ -0,0 +1,18 @@
+
+from flask_app.config.mysqlconnection import connectToMySQL
+
+
+class Ninja:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.age = data['age']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO ninjas (first_name, last_name, age, created_at, updated_at, dojo_id) VALUES (%(fname)s, %(lname)s, %(age)s, NOW(), NOW(),%(dojo_id)s)"
+        return connectToMySQL('dojos_and_ninjas').query_db(query,data)
+
